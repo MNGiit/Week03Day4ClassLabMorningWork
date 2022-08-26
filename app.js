7a706b247152b9047e4c6b3bd24e50d6686159a7
MNGiit <36828920+MNGiit@users.noreply.github.com>
2022-08-26 09:09:20 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,159 @@
+Section 1
+Creating Classes & Factories
+Hamster
+attributes:
+
+owner - string, initially set as an empty string
+name - string, set the name from parameter in constructor method
+price - integer, set as 15
+methods:
+
+wheelRun() - log "squeak squeak"
+eatFood() - log "nibble nibble"
+getPrice() - return the price
+🔴 Hard Mode Save & Commit your work!
+Your commit message should read something like:
+"created hamster class"
+Easy Mode Make sure it works so far
+
+class Hamster {
+  constructor(name){
+    this.owner = ''
+    this.name = name
+    this.price = 15
+  }
+  wheelRun(){
+    console.log('squeak squeak')
+  }
+  eatFood(){
+    console.log('nibble nibble')
+  }
+  getPrice(){
+    return this.price
+  }
+}
+Person
+attributes:
+
+name - set name from parameter in constructor method
+age - initially 0
+height - initially 0
+weight - initially 0
+mood - integer starting at 0 initially
+hamsters - empty array initially
+bankAccount - initially set to 0
+methods:
+
+getName() - returns name
+getAge() - returns age
+getWeight() - returns weight
+greet() - logs a message with person's name
+eat() - increment weight, increment mood
+exercise() - decrement weight
+ageUp() - increment age, increment height, increment weight, decrement mood, increment bank account by 10 (birthday money)
+buyHamster(hamster) - push the hamster object onto the hamster array, increment mood by 10, decrement bankAccount by the value of the hamster (hint: use getPrice())
+🔴 Hard Mode Save & Commit your work!
+Your commit message should read something like:
+"created person class"
+Easy Mode Keep Going and save and run code
+
+class Person {
+  constructor(name){
+    this.name = name
+    this.age = 0
+    this.height = 0
+    this.weight = 0
+    this.mood = 0
+    this.hamsters = []
+    this.bankAccount = 0
+  }
+  getName(){
+    return this.name
+  }
+  getAge(){
+    return this.age
+  }
+  getWeight(){
+    return this.weight
+  }
+  greet(){
+    console.log(`I am ${this.name} hello`)
+  }
+  eat()(
+    this.weight++
+    this.mood++
+  )
+  exercise(){
+    this.weight--
+  }
+  ageUp(){
+    this.age++
+    this.height++
+    this.weight++
+    this.mood--
+    this.bankAccount+=10
+  }
+  buyHamster(hamster){
+    this.hamsters.push(hamster)
+    this.mood+=10
+    this.bankAccount-=hamster.getPrice()
+  }
+}
+Create a Story with your Person class
+Feel free to update or add methods to automate some of these tasks.
+
+Instantiate a new Person named Timmy
+Age Timmy five years
+At this point Timmy's a little bummed. As a precocious child, he feels he's "seen it all" already. Have him eat five times.
+Now Timmy's a little heavier than he wants to be. Kindergarten's coming up and he wants to look good. Have him exercise five times
+Age Timmy 9 years
+Create a hamster named "Gus"
+Set Gus's owner to the string "Timmy"
+Have Timmy "buy" Gus
+Age Timmy 15 years
+Have Timmy eat twice
+Have Timmy exercise twice
+🔴 Hard Mode Save & Commit your work!
+Your commit message should read something like:
+"created timmys story" Easy Mode Keep Going Save and Run Code
+
+const timmy = new Person('Timmy')
+for(let i =0; i < 5; i++){
+  timmy.ageUp()
+}
+for(let i =0; i < 5; i++){
+  timmy.eat()
+}
+for(let i =0; i < 5; i++){
+  timmy.exercise()
+}
+for(let i =0; i < 9; i++){
+  timmy.ageUp()
+}
+const gus = new Hamster('Gus')
+gus.owner = 'Timmy'
+timmy.buyHamster(gus)
+
+for(let i =0; i < 15; i++){
+  timmy.ageUp()
+}
+timmy.eat()
+timmy.eat()
+timmy.exercise()
+timmy.exercise()
+Chef Make Dinners
+class Dinner {
+
+}
+
+class Chef {
+
+}
+Chef should be a factory of Dinner
+Add a constructor to dinner that sets the string properties, appetizer, entree and dessert.
+Add a method on chef that takes three arguments and returns a new Dinner based on those arguments.
+Have the Chef create 3 dinners, log the dinners
+The only thing important here is that student recognizes that the Chef should produce dinners
+No wrong answers as long as the Chef can produce dinners
+Chef should have an array of dinners
+Part 1 is about following instructions, part 2 is purposefully ambiguous, as a dev you need to be able to do both
