a11e21909f65293950754a9e3d2e4928bc280643
MNGiit <36828920+MNGiit@users.noreply.github.com>
2022-08-26 09:21:11 -0400

    add eat() to person

--- app.js
+++ app.js
@@ -44,21 +44,12 @@ class Person {
     greet() {
         console.log(`Hello world, my name is ${this.name}!`);
     }
+
+    eat() {
+        this.weight++;
+        this.mood++;
+    }
 }
-
-// name - set name from parameter in constructor method
-// age - initially 0
-// height - initially 0
-// weight - initially 0
-// mood - integer starting at 0 initially
-// hamsters - empty array initially
-// bankAccount - initially set to 0
-// methods:
-
-// getName() - returns name
-// getAge() - returns age
-// getWeight() - returns weight
-// greet() - logs a message with person's name
 // eat() - increment weight, increment mood
 // exercise() - decrement weight
 // ageUp() - increment age, increment height, increment weight, decrement mood, increment bank account by 10 (birthday money)
